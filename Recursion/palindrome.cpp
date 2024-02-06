7b8591d1ae975c5be1d8c54042554d8623c03ce9
devanshV18 <devanshverma024@gmail.com>
2024-02-07 00:30:48 +0530

    palindrome check

---
+++ Recursion/palindrome.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+bool checkPalindrome(string str, int i, int j){
+    //base case
+    if(i>j){
+        return true;
+    }
+
+    //solving one case
+    if(str[i] != str[j]){
+        
+        return false;
+    }
+
+    else{
+        return checkPalindrome(str,i+1,j-1);
+    }
+}
+
+int main(){
+    string str = "racecar";
+    int res = checkPalindrome(str, 0, str.length()-1);
+    cout<<res<<endl;
+}
