57f3397bce35568684bd998f5dce14c1737de488
devanshV18 <devanshverma024@gmail.com>
2024-02-16 21:22:26 +0530

    creating trees

--- Trees/creatingTreeusingrec.cpp
+++ Trees/creatingTreeusingrec.cpp
@@ -1,42 +1,37 @@
 #include<iostream>
 using namespace std;
 
-class node{
+class node {
     public:
-    int data;
-    node* left;
-    node* right;
+        int data;
+        node* left;
+        node* right;
 
-    node(int d){
+    node(int d) {
         this -> data = d;
         this -> left = NULL;
         this -> right = NULL;
     }
-    public:
-    node* buildTree(node* root){
-    cout<<"Enter the data: "<<endl;
+};
+
+node* buildTree(node* root) {
+
+    cout << "Enter the data: " << endl;
     int data;
-    cin>>data;
-    root = new node(data);
+    cin >> data;
+    root = new node(data);    
 
-    if(data == -1){
-        retrun NULL;
-
+    if(data == -1) {
+        return NULL;
     }
 
-    cout<<"Enter data for inserting left"<<endl;
-
-    root -> left = buildTree(root->left)
-
-    cout<<"Enter data for inserting left"<<endl;
-
-    root -> right = buildTree(root -> right);
+    cout << "Enter data for inserting in left of " << data << endl;
+    root->left = buildTree(root->left);
+    cout << "Enter data for inserting in right of " << data << endl;
+    root->right = buildTree(root->right);
     return root;
- }
-}
- 
- 
 
+}
 
 int main(){
     node* root = NULL;
