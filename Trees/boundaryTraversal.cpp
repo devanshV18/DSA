64d6e9b570fbb14c8060a708faa342db3fc22e5e
devanshV18 <devanshverma024@gmail.com>
2024-03-03 23:46:52 +0530

    right boundary fxn

---
+++ Trees/boundaryTraversal.cpp
@@ -0,0 +1,72 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+class node {
+    public:
+        int data;
+        node* left;
+        node* right;
+
+    node(int d) {
+        this -> data = d;
+        this -> left = NULL;
+        this -> right = NULL;
+    }
+};
+
+node* buildTree(node* root) {
+
+    cout << "Enter the data: " << endl;
+    int data;
+    cin >> data;
+    root = new node(data);    
+
+    if(data == -1) {
+        return NULL;
+    }
+
+    cout << "Enter data for inserting in left of " << data << endl;
+    root->left = buildTree(root->left);
+    cout << "Enter data for inserting in right of " << data << endl;
+    root->right = buildTree(root->right);
+    return root;
+
+}
+bool isLeaf(node* node){
+    if(node->left == NULL && node->right ==NULL){
+        return true;
+    }
+    return false;
+}
+
+void addLeftBoundary(node* root, vector<int> &res){
+    node* curr = root->left;
+
+    while(curr){
+        if(!isLeaf(curr)) res.push_back(curr->data);
+        if(curr->left) curr = curr->left;
+        else curr = curr->right;
+    }
+}
+
+void addRightBoundary(node* root,vector<int> &res){
+    node* curr = root->right;
+    vector<int> temp;
+
+    while(curr){
+        if(!isLeaf(curr)) temp.push_back(curr->data);
+        if(curr->right) curr = curr->right;
+        else curr = curr->left;
+    }
+    for(int i = temp.size()-1 ; i>=0 ; --i){
+        res.push_back(temp[i]);
+    }
+}
+
+
+
+
+int main(){
+
+}
