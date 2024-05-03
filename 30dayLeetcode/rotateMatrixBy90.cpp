763ac7fe5d7d75787cddb8e76acb0ade1f5d63d8
devanshV18 <devanshverma024@gmail.com>
2024-05-03 23:28:12 +0530

    rotate an array by 90 degrees

---
+++ 30dayLeetcode/rotateMatrixBy90.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+#include<bits/stdc++.h>
+
+
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size(), m = matrix[0].size();
+        vector<vector<int>> rotate(n, vector<int>(m));
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++)
+                rotate[j][i] = matrix[i][j];
+        }
+        for(int i=0; i<n; i++)
+            reverse(rotate[i].begin(), rotate[i].end());
+        matrix = rotate;
+    }
+};
