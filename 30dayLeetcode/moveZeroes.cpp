6706a1d724eb8692bac6b95adf08d6715596fe9d
devanshV18 <devanshverma024@gmail.com>
2024-05-04 18:25:56 +0530

    solved move Zeroes in an array Problem

--- 30dayLeetcode/moveZeroes.cpp
+++ 30dayLeetcode/moveZeroes.cpp
@@ -1,3 +1,16 @@
 #include<iostream>
 using namespace std;
 #include<bits/stdc++.h>
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int i = 0;
+        for (int j = 0; j < nums.size(); j++) {
+            if (nums[j] != 0) {
+                swap(nums[j], nums[i]);
+                i++;
+            }
+        }
+    }
+};
