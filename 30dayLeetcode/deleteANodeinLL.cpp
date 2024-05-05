b0b04cdebd0c331ddc1e2c2cf342753b7c52072d
devanshV18 <devanshverma024@gmail.com>
2024-05-06 02:22:06 +0530

    delete a Node in LL

---
+++ 30dayLeetcode/deleteANodeinLL.cpp
@@ -0,0 +1,14 @@
+// #include<iostream>
+// #include<bits/stdc++.h>
+// using namespace std;
+
+// class Solution {
+// public:
+//     void deleteNode(ListNode* node) {
+
+//         ListNode* current = node -> next;
+//         node -> val = current -> val;
+//         node -> next = current -> next;
+
+//     }
+// };
