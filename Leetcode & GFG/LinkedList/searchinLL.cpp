f31f91319b3a9a6ff1ecf7b062f2d0d17d9ce5b9
devanshV18 <devanshverma024@gmail.com>
2024-06-13 22:04:35 +0530

    search in LL

---
+++ Leetcode & GFG/LinkedList/searchinLL.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node* next;
+    Node(int x) {  data = x;  next = NULL; }
+}; 
+
+class Solution {
+  public:
+    // Function to count nodes of a linked list.
+    bool searchKey(int n, struct Node* head, int key) {
+        if(head == NULL){
+            return false;
+        }
+        
+        Node* temp = head;
+        while(temp!=NULL){
+            if(temp->data == key){
+                return true;
+            }
+            temp = temp->next;
+        }
+    }
+};
