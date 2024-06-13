776b43a9e878b659c818d8a84bd41b1c4a1f750a
devanshV18 <devanshverma024@gmail.com>
2024-06-13 22:01:57 +0530

    deleted a given node in a LL without access to head

---
+++ Leetcode & GFG/LinkedList/deletingAnodeinLL.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+
+//  * Definition for singly-linked list.
+struct ListNode {
+int val;
+ ListNode *next;
+ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        ListNode* nextNode = node->next;
+        node->val = nextNode->val;
+        node->next = nextNode -> next;
+
+    }
+};
