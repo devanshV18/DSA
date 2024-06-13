1f1a297d11aa83fb767de47f7d9efb285b048d47
devanshV18 <devanshverma024@gmail.com>
2024-06-13 21:24:35 +0530

    solved missing number in an array and single frequency number problem

---
+++ 30dayLeetcode/missingNumber.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int missingNumber(vector<int> &nums){
+    long long sum = 0;
+
+    for(int i=0; i<nums.size(); i++){
+        sum += i;
+    }
+
+    for(int i=0; i<nums.size(); i++){
+        sum -= nums[i];
+    }
+
+    return sum;
+}
