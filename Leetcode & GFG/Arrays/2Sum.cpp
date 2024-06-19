e66eab2904ae1060191fddc88bbd4aa16736a33c
devanshV18 <devanshverma024@gmail.com>
2024-06-19 15:32:10 +0530

    revised and solved 2 sum

--- Leetcode & GFG/Arrays/2Sum.cpp
+++ Leetcode & GFG/Arrays/2Sum.cpp
@@ -1,28 +1,19 @@
 #include<iostream>
-using namespace std;
 #include<vector>
+#include<unordered_map>
+using namespace std;
 
+vector<int> twoSum(vector<int> nums, int target){
+    unordered_map<int,int> m;
 
-#include <unordered_map>
- 
-class Solution {
-public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        // Declare an unordered map to store the numbers and their indices
-        unordered_map<int, int> mp;
-       
-        // Loop through the array
-        for(int i = 0; i < nums.size(); i++){
-            // Check if the complement of the current number exists in the map
-            if(mp.find(target - nums[i]) == mp.end())
-                // If not, add the current number and its index to the map
-                mp[nums[i]] = i;
-            else
-                // If yes, return the indices of the current number and its complement
-                return {mp[target - nums[i]], i};
+    for(int i = 0; i<nums.size(); i++){
+        if(m.find(target-nums[i]) == m.end()){  //if required number is not in map
+            m[nums[i]] = i; //populate map
         }
- 
-        // If no pair is found, return {-1, -1} as a default value
-        return {-1, -1};
+        else{ // required number is in map
+            return {m[nums[i]],i};  // map - > val index  returns{index for the value target - nums[i] (required number) from the map, current index being traversed}
+        }
     }
-};
+
+    return {-1,-1}; // in case no combination is found
+}
