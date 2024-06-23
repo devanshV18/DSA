656780190db01630d9facc96461453fef63b103e
devanshV18 <devanshverma024@gmail.com>
2024-06-23 16:19:46 +0530

    A constrained optiaml solution for longest consecutive sequence

--- Leetcode & GFG/Arrays/lonestConsecutiveSubSequence.cpp
+++ Leetcode & GFG/Arrays/lonestConsecutiveSubSequence.cpp
@@ -2,6 +2,9 @@
 #include<vector>
 #include<bits/stdc++.h>
 using namespace std;
+#include<unordered_map>
+
+//A fairly optimal solution
 
 int longestSuccesiveSeq(vector<int> &nums){
     if(nums.size() == 0) return 0;
@@ -28,6 +31,36 @@ int longestSuccesiveSeq(vector<int> &nums){
         }
 
         longest = max(cnt, longest);
+    }
+
+    return longest;
+}
+
+// A constarintly optimal solution : -
+
+int longestSubsequence(vector<int> &numss){
+    if(numss.size() == 0) return 0;
+
+    int longest = 1;
+    unordered_set<int> st;
+
+    for(int i = 0; i<numss.size(); i++){
+        st.insert(numss[i]);
+    }
+
+    for(auto it : st){
+        if(st.find(it - 1) == st.end()){
+            int count = 1;
+            int x = it;
+
+            while(st.find(x+1) != st.end()){
+                x = x+1;
+                count += 1;
+            }
+
+            longest = max(count,longest);
+        }
+        
     }
 
     return longest;
