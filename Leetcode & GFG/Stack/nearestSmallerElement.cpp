dea78a5542799d25accaa13134ed93a93ed17704
devanshV18 <devanshverma024@gmail.com>
2024-09-02 23:10:20 +0530

    solved nearest smaller eleemnt problem with a monotonic stack approach

--- Leetcode & GFG/Stack/nearestSmallerElement.cpp
+++ Leetcode & GFG/Stack/nearestSmallerElement.cpp
@@ -21,3 +21,22 @@ vector<int> nse(vector<int> &nums){
     }
     return nse;
 }
+
+//Increasing Monotonic Stack Approach
+
+vector<int> nseOpt(vector<int> &nums){
+    int n = nums.size();
+    vector<int> nse(n,-1);
+    stack<int> st;
+    for(int i = 0; i<n-1; i++){
+        while( !st.empty() && st.top() >= nums[i] ){
+            st.pop();
+        }
+
+        nse[i] = st.empty() ? -1 : st.top();
+
+        st.push(nums[i]);
+    }
+
+    return nse;
+}
