3163ee082e4a5ea7116b1fee33a1e9ec78dd581e
devanshV18 <devanshverma024@gmail.com>
2024-09-12 05:40:44 +0530

    solved deletion of nth node from back using brute force

---
+++ Leetcode & GFG/LinkedList/deletenthfromlast.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+struct ListNode {
+     int val;
+     ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+ };
+
+ //BRUTE FORCE
+
+ ListNode* deletefromLast(ListNode* head, int n){
+    int cnt = 0;
+    ListNode* temp = head;
+    while(temp != NULL){
+        cnt++;
+        temp = temp->next;
+    }
+
+    //edge case - head node
+    if(cnt == n){
+        ListNode* newHead = head->next;
+        return newHead;
+    }
+
+    int res = cnt - n;
+    ListNode* temp = head;
+    while(temp != NULL){
+        res--;
+        if(res == 0){
+            break;
+        }
+        temp = temp->next;
+    }
+    ListNode* delNode = temp->next;
+    temp->next = temp->next->next;
+    return head;
+ }
