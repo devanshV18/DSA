e8f5a22261c9f4d55b763af51f66ecc6e96384c9
devanshV18 <devanshverma024@gmail.com>
2024-10-15 17:32:40 +0530

    debugged the solution for removing k digits

--- Stacks/removeKDigits.cpp
+++ Stacks/removeKDigits.cpp
@@ -13,7 +13,7 @@ string removeKdigits(string num, int k){
     //if while is false -> we simply push the num[i] into the stack
 
     for(int i = 0; i<n; i++){
-        while( !s.empty() && k>0 && ( s.top() - '0' ) > ( s.top() - '0' ) ){
+        while( !s.empty() && k>0 && ( s.top() - '0' ) > ( num[i] - '0' ) ){
             s.pop();
             k--;
         }
@@ -21,7 +21,7 @@ string removeKdigits(string num, int k){
     }
 
     //handling edge case 3 -> if we have a incremental string then no pop occurs as every num[i] > s.top() -> so we might end up adding all elements to stack and k didn't decrease at all, or we might removed some elements but k still isn't zero that means we reached incremental string state midway and some part of the string is incremental rather entire string being incremental -> so we handle this case by simply popping the stack equal to remaining k.
-    
+
     while( k > 0 ){
         s.pop();
         k--;
@@ -43,6 +43,7 @@ string removeKdigits(string num, int k){
     while( !s.empty() && result.back() == '0'){
         result.pop_back();
     }
+    reverse(result.begin(), result.end());
 
     //handling 1st edge case -> k == n where we end up removing everything
     if(result.empty()){
