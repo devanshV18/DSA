3107c20a7830c1cd705ee8cb320ddca186c1199c
devanshV18 <devanshverma024@gmail.com>
2024-10-15 21:00:58 +0530

    solved sum of subarray ranges using brute force method

---
+++ Stacks/subarrayRangeSum.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<vector>
+using namespace std;
+
+int sumSubarrayRanges(vector<int> v){
+    int n = v.size();
+    int sum = 0;
+    for(int i = 0; i<n; i++){
+       int largest = v[i];
+       int smallest = v[i];
+       for(int j = i+1; j<n; j++){
+        largest = max(largest, v[j]);
+        smallest = min(smallest, v[j]);
+
+        sum = sum + (largest - smallest);
+       }
+    }
+    return sum;
+}
+
+int main(){
+    vector<int> test = {1,4,3,2};
+    int ans = sumSubarrayRanges(test);
+
+    cout << ans << endl;
+}
