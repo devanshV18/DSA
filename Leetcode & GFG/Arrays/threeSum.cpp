5b917d20689387010483746c291d0d430caef63a
devanshV18 <devanshverma024@gmail.com>
2024-10-29 06:24:14 +0530

    solved the optimal approach for the three sum problem

--- Leetcode & GFG/Arrays/threeSum.cpp
+++ Leetcode & GFG/Arrays/threeSum.cpp
@@ -49,9 +49,52 @@ vector<vector<int>> threeSumBetter(vector<int> nums){
     return ans;
 }
 
+
+//OPTIMAL APPROACH
+
+vector<vector<int>> threeSumOptimal(vector<int>& nums) {
+        int n = nums.size();
+        vector<vector<int>> ans;
+
+        sort(nums.begin(), nums.end());
+
+        for(int i = 0; i<n; i++){
+            if( i > 0 && nums[i] == nums[i-1] ){
+                continue;
+            }
+
+            int j = i+1;
+            int k = n-1;
+
+            while( j < k ){
+                int sum = nums[i] + nums[j] + nums[k];
+                if( sum < 0 ){
+                    j++;
+                }
+                else if( sum > 0 ){
+                    k--;
+                }
+                else{
+                    vector<int> temp = {nums[i],nums[j],nums[k]};
+                    ans.push_back(temp);
+                    j++;
+                    k--;
+                    while( j < k && nums[j] == nums[j-1] ){
+                        j++;
+                    }
+                    while( j < k && nums[k] == nums[k+1] ){
+                        k--;
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+
+
 int main(){
     vector<int> v = {-1,0,1,2,-1,-4};
-    vector<vector<int>> ans = threeSumBetter(v);
+    vector<vector<int>> ans = threeSumOptimal(v);
 
     //printing a 2d array
 
