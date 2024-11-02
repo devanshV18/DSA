ec2a082941adb14941b89b24dcc88e6795dfb2fa
devanshV18 <devanshverma024@gmail.com>
2024-11-02 21:34:04 +0530

    solved the brutwe and better solution of three sum problem

--- Leetcode & GFG/Arrays/threeSum.cpp
+++ Leetcode & GFG/Arrays/threeSum.cpp
@@ -8,21 +8,23 @@ using namespace std;
 
 vector<vector<int>> threeSum(vector<int> nums){
     int n = nums.size();
-    set<vector<int>> s;
+    set<vector<int>> s; 
+
     for(int i = 0; i<n; i++){
-        for( int j = i+1; j<n; j++){
+        for(int j = i+1; j<n; j++){
             for(int k = j+1; k<n; k++){
-                if(nums[i] + nums[j] + nums[k] == 0){
-                    vector<int> temp = {nums[i], nums[j], nums[k]};
-                    sort(temp.begin(), temp.end());
-                    s.insert(temp);
+                // the triplet combination
+                if(nums[i] + nums[j] + nums[k] == 0 ) {
+                    vector<int> temp = {nums[i], nums[j], nums[k]}; //formed a temporary triplet
+                    sort(temp.begin(), temp.end()); //sorted the triplet to compare each triplet and all unique triplets are stored in the set
+                    s.insert(temp); // the triplet temp is inserted in the set uniquely             
                 }
             }
         }
     }
 
-    vector<vector<int>> ans(s.begin(), s.end());
-    return ans;
+    vector<vector<int>> ans(s.begin(), s.end()); //the list of list stores all list (unique triplets) from set s into the ans list of lists 
+    return ans; //return the answer simply.
 }
 
 
@@ -33,18 +35,21 @@ vector<vector<int>> threeSumBetter(vector<int> nums){
     set<vector<int>> s;
 
     for(int i = 0; i<n; i++){
-        set<int> hashSet;
+        set<int> hashset; // we initiate an empty hashset as i moves or say for every new starting point i, as it subsequently updates j and hence both i and j moves, so a new hashset everytime i and j moves enables us to hash elements only between i and j thus avoiding any repeatition
+
         for(int j = i+1; j<n; j++){
             int third = -(nums[i] + nums[j]);
-            if(hashSet.find(third) != hashSet.end()){
-                vector<int> temp = {nums[i], nums[j], third};
+            if(hashset.find(third) != hashset.end()){
+                //if third is in the hashset, we got a triplet
+                vector<int> temp = { nums[i], nums[j], third};
                 sort(temp.begin(), temp.end());
                 s.insert(temp);
             }
-
-            hashSet.insert(nums[j]);
+            //populate the hashset for every iteration
+            hashset.insert(nums[j]);
         }
     }
+
     vector<vector<int>> ans(s.begin(), s.end());
     return ans;
 }
@@ -93,8 +98,8 @@ vector<vector<int>> threeSumOptimal(vector<int>& nums) {
 
 
 int main(){
-    vector<int> v = {-1,0,1,2,-1,-4};
-    vector<vector<int>> ans = threeSumOptimal(v);
+    vector<int> v = {0,0,0};
+    vector<vector<int>> ans = threeSumBetter(v);
 
     //printing a 2d array
 
