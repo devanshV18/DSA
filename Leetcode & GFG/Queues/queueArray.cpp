bd2e7cb8bf483b91cd0730c30d2491de4469c645
devanshV18 <devanshverma024@gmail.com>
2024-11-07 04:26:47 +0530

    queue imp revision

--- Leetcode & GFG/Queues/queueArray.cpp
+++ Leetcode & GFG/Queues/queueArray.cpp
@@ -2,68 +2,67 @@
 using namespace std;
 
 class Queue{
-    int capacity;
-    int *q;
-    int cs;
-    int s;
-    int e;
 
+public:
+    int capacity;
+    int* q;
+    int cs;
+    int s
+    int e;
+    
     Queue(int size){
         capacity = size;
         q = new int[size];
         cs = 0;
-        s = e = -1; 
+        s=e=-1;
     }
 
-    void push(int val){
-        //Full queue
-        if( cs == capacity){
-            cout << "Queue if FULL";
+    void enqueue(int x){
+        if( cs == capacity ){
+            cout << "Queue if Full";
         }
 
-        //empty queue
-        if(cs == 0){
+        //if not full we can enque
+        //case 1 -> empty queue -> both s and e  should move.
+        if( cs == 0 ){
             s = 0;
             e = 0;
         }
-        //edge case and Normal trivial case handled together
         else{
             e = (e+1) % capacity;
         }
-        q[e] = val;
+        q[e] = x;
         cs++;
     }
 
-    int pop(){
-        //Empty case
-        if(cs == 0){
-            cout<< "Queue is Empty" <<endl;
+    void dequeue(){
+        if( cs == 0 ){
+            cout << "Queue is Empty" << endl;
         }
 
-        int popel = q[s];
-
-        //Queue destruction case
-        if(cs == 1){
+        //queue destruction case
+        if( cs == 1 ){
             s=e=-1;
         }
-
-        //remaining trivial case
         else{
             s = (s+1) % capacity;
         }
-
-        cs -= 1;
-        return popel;
+        cs--;
     }
 
-    int top(){
-        if(cs == 0){
-            cout<<"Queue is Empty"<<endl;
+    int front(){
+        if( cs == 0 ){
+            cout << "Queue is Empty" << endl;
         }
+
         return q[s];
     }
 
     int size(){
         return cs;
     }
-};
+}
+
+//cases to handle:- 
+//1. Pushing into an empty queue -> when queue is empty both s and e moves to 0 from -1 and then push occurs and cs++ else only e moves as per modulus operator.
+//2. while popping(deque) if cs==0 || s==e it means the element you are dequeing is the last and this dequeu destroys the queue, thus to handle this, s=e=-1, if its not the last elemet simply increment s by the modulus operator.
