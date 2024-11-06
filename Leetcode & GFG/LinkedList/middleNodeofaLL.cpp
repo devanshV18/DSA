0d99d0c20bd5c891bab562d818e631284fd69e82
devanshV18 <devanshverma024@gmail.com>
2024-11-07 01:06:54 +0530

    middle node in a sll

--- Leetcode & GFG/LinkedList/middleNodeofaLL.cpp
+++ Leetcode & GFG/LinkedList/middleNodeofaLL.cpp
@@ -13,10 +13,11 @@ struct ListNode {
 class Solution {
 public:
     ListNode* middleNode(ListNode* head) {
-        ListNode* fast = head;
-        ListNode* slow = head;
 
-        while(fast != NULL && fast->next != NULL){
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while( fast!=NULL && fast->next != NULL){
             slow = slow->next;
             fast = fast->next->next;
         }
