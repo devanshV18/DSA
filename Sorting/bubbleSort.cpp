06994f9017285f9627e4e080bdf67e7edcd228c6
devanshV18 <devanshverma024@gmail.com>
2024-11-13 09:11:22 +0530

    done with bubble sort as well

--- Sorting/bubbleSort.cpp
+++ Sorting/bubbleSort.cpp
@@ -2,21 +2,20 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+//in bubble sort, we do adjacent swapping starting from first and second index, and it goes till last and second last index, inorder to implememt this, we make the i pointer start from right side to mark a point before i, till where j should move and j starts from zero and goes till one index before i (for each i) and make adjacent swaps. After one complete pass of n'th adjacent swap the n'th largest element is at the n'th place from right.
+//i.e. after completion of first adjacent apss completely the 1st maximum or simply the maximum element is placed at the 1st place from right i.e. last index.
 
-vector<int> bubbleSort(vector<int> &v){
+//NOTE -> for each i -> we complete an entire pass of adjacent swapping.
 
+void bubbleSort(vector<int> &v){
     int n = v.size();
-
-    for(int i = n-1; i>=0; i--){
-        for(int j = 0; j<= i-1; j++){
+    for(int i = n-1; i>=0; i--){ 
+        for(int j = 0; j<=i-1; j++){ //j goes till one place before where i is or j < i
             if(v[j] > v[j+1]){
-                int temp = v[j+1];
-                v[j+1] = v[j];
-                v[j] = temp;
+                swap(v[j],v[j+1]);
             }
         }
     }
-    return v;
 }
 
 int main(){
