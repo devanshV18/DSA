3da7f0ff666e2725eb858da585be9b2b3eba7c42
devanshV18 <devanshverma024@gmail.com>
2024-11-13 08:59:10 +0530

    revised selection sort

--- Sorting/selectionSort.cpp
+++ Sorting/selectionSort.cpp
@@ -2,16 +2,18 @@
 #include<vector>
 using namespace std;
 
-
+//in selection sort, we iterate through the entire array towards the righ in the inner loop (j) for each value of i until i < n-1, i can go upto second last elemnt. The inner loop compares each element starting from element at ith index till the end, and if any elemnt is lesser than current ith, minIndex is updated to that index.
+// once inner loop ends, we swap v[minIndex, v[i]], swapping the minimum elememt to the start of the array.
 void selectionSort(vector<int> &v){
     int n = v.size();
-    for( int i = 0; i< n-1; i++ ){
-        int minIndex = i;
-        for(int j = i; j<n; j++){
+    for(int i = 0; i<n-1; i++){ //i goes till second last elemnt
+        int minIndex = i; //for each new i we call minIndex to be the first element or i.
+        for(int j = i; j<n; j++){ //j goes till last
             if(v[j] < v[minIndex]){
                 minIndex = j;
             }
-        }
+        }//inner loop
+
         swap(v[i], v[minIndex]);
     }
 }
