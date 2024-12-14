626abdb18c7a60b0d6c0773c1b1b7166b5d09dec
devanshV18 <devanshverma024@gmail.com>
2024-12-14 21:04:55 +0530

    invert tree

---
+++ Trees/invertTree.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+
+class node {
+    public:
+        int data;
+        node* left;
+        node* right;
+
+    node(int d) {
+        this -> data = d;
+        this -> left = NULL;
+        this -> right = NULL;
+    }
+};
+
+TreeNode* invertTree(TreeNode* root) {
+        if (root == nullptr) {
+            return nullptr;
+        }
+        
+        TreeNode* temp = root->left;
+        root->left = root->right;
+        root->right = temp;
+        
+        invertTree(root->left);
+        invertTree(root->right);
+        
+        return root;        
+    }
