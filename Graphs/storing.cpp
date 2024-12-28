3146ddf958fd5f963dec4045f7f3c77d4b2ec108
Devansh Verma <143462246+devanshV18@users.noreply.github.com>
2024-12-28 20:45:36 +0530

    implemented all graph storages with all variations

--- Graphs/storing.cpp
+++ Graphs/storing.cpp
@@ -4,15 +4,14 @@ using namespace std;
 
 int main(){
 
-    // execute any 1
-
-    //adjacency matrix method
+   
+    //1. adjacency matrix method -> Undirected graph
 
     int n, m;
     cin >> n >> m;
 
     //for 1 based indexing of graphs
-    vector<vector<int>> adj(n+1, m+1);
+   int adj[n+1][m+1];
 
     //note-> m represents edges, so we iterate from i = 0 till m (just before m) which is eaxctly equal to m
     for(int i = 0; i<m; i++){
@@ -22,8 +21,23 @@ int main(){
         adj[v][u] = 1;
     }
 
+    //2. adjacency matrix method -> Directed graph
 
-    //adjacency list method;
+    int n, m;
+    cin >> n >> m;
+
+    //for 1 based indexing of graphs
+   int adj[n+1][m+1];
+
+    //note-> m represents edges, so we iterate from i = 0 till m (just before m) which is eaxctly equal to m
+    for(int i = 0; i<m; i++){
+        int u, v;
+        cin >> u >> v;
+        adj[u][v] = 1;
+    }
+
+
+    //3. adjacency list method -  Undirected graph
 
     int n,m;
     cin >> n >> m;
@@ -36,4 +50,80 @@ int main(){
         adj[u].push_back(v);
         adj[v].push_back(u);
     }
+
+    //4. adjacency list method -  Directed graph
+
+    int n,m;
+    cin >> n >> m;
+
+    vector<int> adj[n+1];
+
+    for(int i = 0; i<m; i++){
+        int u,v;
+        cin >> u >> v;
+        adj[u].push_back(v);
+    }
+
+
+    //5. adjacency matrix method -> weighted undirected graph
+
+    int n, m;
+    cin >> n >> m;
+
+    //for 1 based indexing of graphs
+   int adj[n+1][m+1];
+
+    //note-> m represents edges, so we iterate from i = 0 till m (just before m) which is eaxctly equal to m
+    for(int i = 0; i<m; i++){
+        int u, v, wt
+        cin >> u >> v >> wt;
+        adj[u][v] = wt;
+        adj[v][u] = wt;
+    }
+
+
+    //6. adjacncy list method -> weighted undirected graph
+
+    int n,m;
+    cin >> n >> m;
+
+    vector<vector<pair<int, int>>> adj(n + 1);
+
+    for(int i = 0; i<m; i++){
+        int u,v,wt;
+        cin >> u >> v >> wt;
+        adj[u].push_back({v,wt});
+        adj[v].push_back({u,wt});
+    }
+
+    //7. adajcency matrix method -> weighted directed
+
+    int n, m;
+    cin >> n >> m;
+
+    // Adjacency matrix for a 1-based indexing graph
+    vector<vector<int>> adj(n + 1, vector<int>(n + 1, 0)); // Initialize with 0 (no edge)
+
+    for (int i = 0; i < m; i++) {
+        int u, v, wt;
+        cin >> u >> v >> wt;
+        adj[u][v] = wt; // Add edge u -> v with weight wt
+    }
+
+    //8. adjacncy list method -> weighted directed graph
+
+    int n,m;
+    cin >> n >> m;
+
+    vector<vector<pair<int, int>>> adj(n + 1);
+
+    for(int i = 0; i<m; i++){
+        int u,v,wt;
+        cin >> u >> v >> wt;
+        adj[u].push_back({v,wt});
+    }
+
+
+
+
 }
