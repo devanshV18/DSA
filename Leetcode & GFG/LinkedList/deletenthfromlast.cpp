17acf1e33358891154b4c9f516c745bc892aeb6b
Devansh Verma <143462246+devanshV18@users.noreply.github.com>
2025-01-11 05:51:21 +0530

    solved delete nth node from last in a LL optimally.

--- Leetcode & GFG/LinkedList/deletenthfromlast.cpp
+++ Leetcode & GFG/LinkedList/deletenthfromlast.cpp
@@ -9,57 +9,60 @@ struct ListNode {
  };
 
  //BRUTE FORCE
-
+ //1 based indexing code
  ListNode* deletefromLast(ListNode* head, int n){
+    if(head == NULL){
+        return head;
+    }
+
     int cnt = 0;
     ListNode* temp = head;
+
     while(temp != NULL){
         cnt++;
         temp = temp->next;
     }
 
-    //edge case - head node
-    if(cnt == n){
-        ListNode* newHead = head->next;
-        return newHead;
+    if(n > cnt || n<= 0){
+        return head;
+    }
+
+    if(n == cnt ){
+        return head->next;
     }
 
     int res = cnt - n;
-    ListNode* temp = head;
-    while(temp != NULL){
-        res--;
-        if(res == 0){
-            break;
-        }
+    temp = head;
+
+    for(int i = 1; i<res; i++){
         temp = temp->next;
     }
-    ListNode* delNode = temp->next;
-    temp->next = temp->next->next;
+
+    ListNode* nodeToDelete = temp->next;
+    temp->next = nodeToDelete->next;
+    nodeToDelete->next = NULL;
+
     return head;
  }
 
- //OPTIMISED
+ ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode* fast = head;
+        for(int i = 0; i<n; i++){
+            fast = fast->next;
+        }
 
- ListNode* deleteNthFromLat(ListNode* head, int n){
-    ListNode* fast  =head;
-    for(int i = 0; i<n; i++){
-        fast = fast->next;
+        if(fast == NULL){
+            ListNode* newHead = head->next;
+            return newHead;
+        }
+
+        ListNode* slow = head;
+        while(fast!= NULL && fast->next != NULL){
+            slow = slow->next;
+            fast=fast->next;
+        }
+
+        ListNode* del = slow->next;
+        slow->next = slow->next->next;
+        return head;
     }
-
-    //edge case - head node
-    if(fast == NULL){
-        ListNode* newHead = head->next;
-    }
-
-    ListNode* slow = head;
-    while(fast->next != NULL){
-        fast = fast->next;
-        slow = slow->next;
-    }
-
-    ListNode* delNode = slow->next;
-    slow->next = slow->next->next;
-    return head; 
-
-    
- }
