def2a7861450128509e1a472dca9baa7315ac611
Devansh Verma <143462246+devanshV18@users.noreply.github.com>
2025-01-17 17:15:09 +0530

    revised remove duplicates from a Doubly Linked list problem

--- Leetcode & GFG/LinkedList/removeDuplicatedFromDLL.cpp
+++ Leetcode & GFG/LinkedList/removeDuplicatedFromDLL.cpp
@@ -34,3 +34,25 @@ Node* removedDuplicates(Node* head){
 
      return head;
 }
+
+Node* removeDuplicatesfromDLL(Node* head){
+    Node* temp = head;
+
+    //the condition for the while loop to work.
+    while(temp!= NULL && temp->next != NULL){
+        Node* nextNode = temp->next;
+        //the below condition is chosen, as nextNode can reach null as well in the process of traversing duplicates, so if nextNode only moves until it is not null and the data at nextNode is equal to the temp->data. If ay any point the data is differed we get a unique node and in case nextNode reaches NULL we again get a differed node say end of the list.
+        while(nextNode != NULL && nextNode->data == temp->data){
+            Node* duplicate = nextNode;
+            nextNode = nextNode->next;
+            delete(duplicate);
+        }
+
+        //when internal while loop terminates, we have reached either a unique node or the end of the list, hence further logic is handled as:-
+
+        temp->next = nextNode;
+        if(nextNode != NULL) nextNode->prev = temp;
+
+        temp = temp->next;
+    }
+}
