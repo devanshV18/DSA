7278c18e45ce0b484e86556d2b58868b5ebe4b75
Devansh Verma <143462246+devanshV18@users.noreply.github.com>
2025-02-17 14:52:19 +0530

    solved search 2d matrix problem based on binary search

---
+++ BinarySearch/Implement.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int binarySearch(vector<int>& arr, int target) {
+    int left = 0;
+    int right = arr.size() - 1;
+
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+
+        if (arr[mid] == target) {
+            return mid; // Target found
+        } else if (arr[mid] < target) {
+            left = mid + 1; // Search in the right half
+        } else {
+            right = mid - 1; // Search in the left half
+        }
+    }
+
+    return -1; // Target not found
+}
+
+int main() {
+    vector<int> arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int target = 5;
+
+    int result = binarySearch(arr, target);
+
+    if (result != -1) {
+        cout << "Element found at index " << result << endl;
+    } else {
+        cout << "Element not found" << endl;
+    }
+
+    return 0;
+}
---
+++ BinarySearch/searchInAMatrix.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+#include<vector>
+
+bool binarySearch(vector<int>& arr, int target) {
+    int left = 0;
+    int right = arr.size() - 1;
+
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+
+        if (arr[mid] == target) {
+            return true; // Target found
+        } else if (arr[mid] < target) {
+            left = mid + 1; // Search in the right half
+        } else {
+            right = mid - 1; // Search in the left half
+        }
+    }
+
+    return false; // Target not found
+}
+
+bool searchInAMatrix(vector<vector<int>> &mat, int target){
+    int m = mat.size();
+    int n = mat[0].size();
+
+    for(int i = 0; i<m; i++){
+        if(target >= mat[i][0] && target <= mat[i][n-1]){
+            return binarySearch(mat[i], target);
+        }
+    }
+
+    return false;
+}
