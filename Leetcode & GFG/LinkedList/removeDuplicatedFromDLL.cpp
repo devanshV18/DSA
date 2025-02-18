110f7276a05931ad58dd3bd76746a43e4bf3e9e1
Devansh Verma <143462246+devanshV18@users.noreply.github.com>
2025-02-18 14:17:42 +0530

    solved the problem removing duplicates from a doub;ly linked list

--- Leetcode & GFG/LinkedList/removeDuplicatedFromDLL.cpp
+++ Leetcode & GFG/LinkedList/removeDuplicatedFromDLL.cpp
@@ -16,43 +16,26 @@ struct Node
         
 };
 
-Node* removedDuplicates(Node* head){
+Node* removeDuplicatesformDLL(Node* head){
+    if(head == NULL || head->next == NULL){
+        return head;
+    }
 
-     Node* temp = head;
-
-     while( temp != NULL && temp->next != NULL ){
-        Node* nextNode = temp->next;
-        while( nextNode != NULL && nextNode->data == temp->data ){
-            Node* duplicate = nextNode;
-            nextNode = nextNode->next;
-            delete(duplicate);
-        }
-        temp->next = nextNode;
-        if(nextNode) nextNode->prev = temp;
-        temp = temp->next;
-     }
-
-     return head;
-}
-
-Node* removeDuplicatesfromDLL(Node* head){
     Node* temp = head;
 
-    //the condition for the while loop to work.
-    while(temp!= NULL && temp->next != NULL){
+    while(temp != NULL && temp->next != NULL){
         Node* nextNode = temp->next;
-        //the below condition is chosen, as nextNode can reach null as well in the process of traversing duplicates, so if nextNode only moves until it is not null and the data at nextNode is equal to the temp->data. If ay any point the data is differed we get a unique node and in case nextNode reaches NULL we again get a differed node say end of the list.
         while(nextNode != NULL && nextNode->data == temp->data){
             Node* duplicate = nextNode;
             nextNode = nextNode->next;
             delete(duplicate);
         }
 
-        //when internal while loop terminates, we have reached either a unique node or the end of the list, hence further logic is handled as:-
-
         temp->next = nextNode;
-        if(nextNode != NULL) nextNode->prev = temp;
-
+        if(nextNode){
+            nextNode->prev = temp;
+        }
         temp = temp->next;
-    }
+    }   
+    return head;
 }
